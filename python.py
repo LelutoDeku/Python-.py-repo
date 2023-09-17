8f0b0286ac07161b2cee435ef2675d1fe501ac81
Leluto Deku <hammadsiddiqui1212@gmail.com>
2023-09-17 18:01:28 +0530

    Guess game with tries

--- python.py
+++ python.py
@@ -21,3 +21,22 @@
 #         break
 # else:
 #     print('Try Again')
+
+
+# secret_number = 9
+# guess_count = 0;
+# guess_limit = 3;
+# while(guess_count < guess_limit):
+#     guess = int(input('Guess: '))
+#     guess_count = guess_count + 1
+#     if guess == secret_number :
+#         print('You Won!')
+#         break
+#     elif (guess_count == guess_limit):
+#         print('You Lose!')
+#         print("Want to try again?(T/F)")
+#         tries = input()
+#         if(tries == 'T' or tries == 't'):
+#             guess_count = 0
+#         else:
+#             break
