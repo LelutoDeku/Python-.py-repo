00d7ef4ad2f7b0542d73e782dd8e962684337564
Leluto Deku <hammadsiddiqui1212@gmail.com>
2023-09-18 15:07:21 +0530

    Added emoji_converter

--- python.py
+++ python.py
@@ -124,9 +124,24 @@
 # print ("The unique values are: ", newlist)
 
 # phone-number dictionary
-phoneBook = {"1":"One", '2':"Two", '3':"Three", '4':"Four", '5':"Five", '6':"Six", '7':"Seven", '8':"Eight", '9':"Nine", '0':"Zero" }
-number  = input("Phone ")
-output = ""
-for digit in number:
-    output = phoneBook.get(digit, "!")
-    print(output, end = " ")
+# phoneBook = {"1":"One", '2':"Two", '3':"Three", '4':"Four", '5':"Five", '6':"Six", '7':"Seven", '8':"Eight", '9':"Nine", '0':"Zero" }
+# number  = input("Phone ")
+# output = ""
+# for digit in number:
+#     output = phoneBook.get(digit, "!")
+#     print(output, end = " ")
+
+def emoji_converter(msg):
+    words = msg.split()
+    emojis = {
+        ":)" : "😃",
+        ":(" : "🙁"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word)
+    return output
+
+
+msg = input("> ")
+print(emoji_converter(msg))
